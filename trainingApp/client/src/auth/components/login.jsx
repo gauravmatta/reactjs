2c67a2aaf427db1cb25f4df5b05f597f78335907
Gaurav Matta <gaurav.matta.tech@gmail.com>
2025-08-11 15:22:54 +0530

    Added Login component register component Root Router and Auth Router

---
+++ trainingApp/client/src/auth/components/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <>
+    Login
+    </>
+  )
+}
+
+export default login
---
+++ trainingApp/client/src/auth/components/register.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const register = () => {
+  return (
+    <>
+    Register
+    </>
+  )
+}
+
+export default register
